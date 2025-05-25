00f30e6a65d89bbc17e85e629e2bd2d8d6183e43
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-05-25 18:16:35 +0100

    Tweak title of app

--- src/hooks/utility/usePageTitle.tsx
+++ src/hooks/utility/usePageTitle.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const usePageTitle = (title: string, includeAppName = true) => {
   useEffect(() => {
-    const appName = "Organisey";
+    const appName = "Salary Split";
     document.title = includeAppName ? `${title} | ${appName}` : title;
   }, [title, includeAppName]);
 };
