ddcb65331a7cd5742fd1ef9330f8be1486655ed3
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-05-25 17:42:07 +0100

    Set up dark mode context

--- src/components/layout/SharedLayout/SharedLayout.tsx
+++ src/components/layout/SharedLayout/SharedLayout.tsx
@@ -2,23 +2,28 @@ import { Outlet } from "react-router-dom";
 
 import { LoadingOverlay } from "@/components/ui/loading-overlay";
 import { Toaster } from "@/components/ui/sonner";
+import { useTheme } from "@/contexts/Theme/ThemeContext";
 import useUser from "@/hooks/user/useUser";
 
 const SharedLayout = () => {
   const { user, fetchingUser } = useUser();
+  const { darkMode } = useTheme();
 
   if (fetchingUser && !user) {
     return <LoadingOverlay fullPage />;
   }
 
   return (
-    <>
-      <main className="min-h-screen">
+    <div
+      className={`min-h-screen bg-background text-foreground ${
+        darkMode ? "dark" : ""
+      }`}
+    >
+      <main>
         <Outlet />
       </main>
-
       <Toaster />
-    </>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import { ThemeProvider } from "./contexts/Theme/ThemeContext.tsx";
 import TanstackProvider from "./tanstackQuery/TanstackProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TanstackProvider>
-      <App />
-    </TanstackProvider>
+    <ThemeProvider>
+      <TanstackProvider>
+        <App />
+      </TanstackProvider>
+    </ThemeProvider>
   </StrictMode>
 );
